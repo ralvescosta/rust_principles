1c4c18b5c45e5c65792781223a67c2cea6165613
Rafael Alves <rafael.rac.mg@gmail.com>
2021-04-28 05:11:31 -0300

    feat: loops

--- section_3/src/main.rs
+++ section_3/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    tuples();
+    For();
 }
 
 fn first_example(){
@@ -58,5 +58,32 @@ fn tuples() -> (i32, f64, char) {
     println!("The first is: {:?}", tup.0);
     println!("The second is: {:?}", tup.1);
     println!("The third is: {:?}", tup.2);
+
     return tup;
+}
+
+fn Loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn For(){
+    let a = [10,20,30,40,50];
+
+    for element in a.iter(){
+        println!("The value is {}", element);
+    }
+
+    for number in (1..4).rev(){
+        println!("{}!", number);
+    }
 }
