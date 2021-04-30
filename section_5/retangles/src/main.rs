13c61015a64f1a4089c9f1f4f34dff9d7adda3fa
Rafael Alves <rafael.rac.mg@gmail.com>
2021-04-30 15:57:10 -0300

    impl

--- section_5/retangles/src/main.rs
+++ section_5/retangles/src/main.rs
@@ -1,18 +1,36 @@
+use std::iter::successors;
+
 
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 30
     };
+    println!("The area of the rectangle {:?} is {} square pixels.", rect1, rect1.area());
 
-    let result = area_with_struct(&rect1);
-
-    println!("The area of the rectangle {:?} is {} square pixels.", rect1, result);
+    let rect2 = Rectangle::square(10);
 }
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
 }
 
 fn area_with_tuples(dimensions: (u32, u32)) -> u32 {
