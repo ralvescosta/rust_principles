1cae97df6c38abf336fa3ab79cc09901a3daa14f
Rafael Alves <rafael.rac.mg@gmail.com>
2021-05-10 20:24:14 -0300

    struct and enum pub

--- section_7/restaurant/src/lib.rs
+++ section_7/restaurant/src/lib.rs
@@ -1,15 +1,48 @@
+fn server_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad
+    }
+}
+
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
 
-    mod serving {
-        fn take_order() {}
+    fn cook_order() {}
 
-        fn serve_order() {}
-
-        fn take_payment() {}
+    fn fix_incorrect_order() {
+        cook_order();
+        super::server_order();
     }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
 }
