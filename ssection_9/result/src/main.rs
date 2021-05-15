6ed44d36c2266be9b291a018ed18d70a82606c31
Rafael Alves <rafael.rac.mg@gmail.com>
2021-05-15 16:21:53 -0300

    panic

--- ssection_9/result/src/main.rs
+++ ssection_9/result/src/main.rs
@@ -1,10 +1,13 @@
-use std::io::Read;
+use std::{fs, io::Read};
 use std::{fs::File};
 use std::io::ErrorKind;
 use std::io;
 
 fn main() {
-    open_file_expect();
+    match read_username_and_propagate_error() {
+        Err(e) => println!("{:?}", e),
+        _ => return,
+    };
 }
 
 fn open_file() {
@@ -55,7 +58,7 @@ fn open_file_expect() {
     let f = File::open("hello.txt").expect("Oi eu sou Goku");
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
+fn read_username_and_propagate_error() -> Result<String, io::Error> {
     let f = File::open("hello.txt");
 
     let mut f = match f {
@@ -76,4 +79,15 @@ fn read_username_from_file_with_question_marker() -> Result<String, io::Error> {
     let mut s = String::new();
     f.read_to_string(&mut s)?;
     Ok(s)
+}
+
+fn read_username_from_file_with_question_marker_smaller() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_with_question_marker_shorter() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
 }
